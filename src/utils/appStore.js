1f95d069dc4cacb8398d5203686d5e3ccda0993b
Ansonsd5 <anson11sd@gmail.com>
2024-01-31 23:06:46 +0530

    feat : tamplate slice

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import formReducer from './formSlice';
+import templateReducer from './templateSlice';
 
 
 const appStore =  configureStore({
     reducer: {
         user : userReducer,
         form: formReducer,
+        template : templateReducer
 
     },
   })
