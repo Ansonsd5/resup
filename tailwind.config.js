30f31fe5e3e0c121347e672c49cbf16ca4391c7a
Ansonsd5 <anson11sd@gmail.com>
2024-02-11 15:32:08 +0530

    fix: congig changes for tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{js,}",],
+  content: [ "./src/**/*.{js,}",
+  "./public/index.html",
+],
   theme: {
     extend: {},
   },
